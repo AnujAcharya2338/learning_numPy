69e0bef4c635c6f41ae46a158cbb1b2213e587fa
AnujAcharya2338 <anujacharya877@gmail.com>
2026-03-09 13:46:30 +0545

    Basic Numpy tutorials

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# print(np.__version__)
+
+# array = np.array([1,2,3,4])
+
+# array = array * 2 
+
+# print(array)
+
+# array = np.array('A')
+
+# array = np.array([1,2,3])
+
+# array + 1
+
+# print(array)
+ 
+
+# radii = np.array([1,2,3])
+
+# print( np.pi * radii ** 2)
+
+
+# array1 = np.array([1,2,3])
+# array2 = np.array([4,5,6])
